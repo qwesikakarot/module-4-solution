ddf51b9d04693e0dbf82f2af4f8f2753ed7e2236
Kevin <57841753+qwesikakarot@users.noreply.github.com>
2022-06-20 21:14:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,18 +57,21 @@ WARNING!!! WARNING!!!
   // name in the loop.
 
   (function () {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", 
-    "Larry", "Paula", "Laura", "Jim"];
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
   
-      for (var index = 0; index < names.length; i++) {
-  
-        var firstLetter = names[index].charAt(0).toLowerCase();
-        if (firstLetter === "j") {
-        
-          byeSpeaker.speak(names[index]);
-        } else {
-      
-          helloSpeaker.speak(names[index]);
+    for (var i = 0; i < names.length; i++) {
+    
+
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+     
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
       }
     }
-  })();
+    
+    })();
